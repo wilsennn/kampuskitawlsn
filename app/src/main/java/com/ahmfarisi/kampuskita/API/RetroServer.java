f4be6f325050e621e484a19cb05cddc194652902
wilsen <wilsenfendrius@mhs.mdp.ac.id>
2023-05-17 12:29:29 +0700

    updt

--- app/src/main/java/com/ahmfarisi/kampuskita/API/RetroServer.java
+++ app/src/main/java/com/ahmfarisi/kampuskita/API/RetroServer.java
@@ -1,8 +1,19 @@
 package com.ahmfarisi.kampuskita.API;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
     private static final String alamatServer = "https://kampuskitawlsn.000webhostapp.com/";
-    private Retrofit
+    private static Retrofit retro;
+
+    public static Retrofit konekRetrofit(){
+        if(retro == null){
+            retro = new Retrofit.Builder()
+                    .baseUrl(alamatServer)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retro
+    }
 }
