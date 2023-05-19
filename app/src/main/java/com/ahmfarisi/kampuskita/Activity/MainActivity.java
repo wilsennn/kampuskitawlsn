b3095862a212a13abdc1703395fcbcdba21467a2
wilsen <wilsenfendrius@mhs.mdp.ac.id>
2023-05-19 15:15:09 +0700

    update

--- app/src/main/java/com/ahmfarisi/kampuskita/Activity/MainActivity.java
+++ app/src/main/java/com/ahmfarisi/kampuskita/Activity/MainActivity.java
@@ -6,15 +6,25 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.service.controls.actions.FloatAction;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.ahmfarisi.kampuskita.API.APIRequestData;
+import com.ahmfarisi.kampuskita.API.RetroServer;
+import com.ahmfarisi.kampuskita.Adapter.AdapterKampus;
 import com.ahmfarisi.kampuskita.Model.ModelKampus;
+import com.ahmfarisi.kampuskita.Model.ModelResponse;
 import com.ahmfarisi.kampuskita.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvKampus;
@@ -38,7 +48,38 @@ public class MainActivity extends AppCompatActivity {
         rvKampus.setLayoutManager(lmKampus);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        retrieveKampus();
+    }
+
     public void retrieveKampus(){
-        pbKampus
+        pbKampus.setVisibility(View.VISIBLE);
+
+        APIRequestData API = RetroServer.konekRetrofit().create(APIRequestData.class);
+        Call<ModelResponse> proses = API.ardRetrieve();
+
+        proses.enqueue(new Callback<ModelResponse>() {
+            @Override
+            public void onResponse(Call<ModelResponse> call, Response<ModelResponse> response) {
+                String kode = response.body().getKode();
+                String pesan = response.body().getPesan();
+                listKampus = response.body().getData();
+
+                adKampus = new AdapterKampus(MainActivity.this, listKampus);
+                rvKampus.setAdapter(adKampus);
+                adKampus.notifyDataSetChanged();
+
+                pbKampus.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onFailure(Call<ModelResponse> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "gagal menghubungi server!",Toast.LENGTH_SHORT).show();
+                pbKampus.setVisibility(View.GONE);
+            }
+        });
     }
 }
